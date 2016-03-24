55cb43a194236a698539fa663ba9260fd1549ec1
Bri Popson and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-03-24 11:51:41 -0700

    add functioning progress bar to selectedKeg

--- app/show-keg-progress.component.ts
+++ app/show-keg-progress.component.ts
@@ -8,9 +8,15 @@ import { EditKegDetailsComponent } from './edit-keg-details.component';
   directives: [EditKegDetailsComponent],
   template:`
   <div class="kegProgress">
-     <h3>{{ keg.brewerName }}'s {{ keg.beerName }} Progress: {{ (keg.kegLevel/127*100).toPrecision(3) }} % </h3>
+     <h3>{{ keg.brewerName }}'s {{ keg.beerName }} Progress: {{ (keg.kegLevel/127*100).toFixed(0) }} % </h3>
      <button (click)="reduceByPint()" class="btn btn-info">Sold a pint?</button>
      <button (click)="reduceByGrowler()" class="btn btn-info">Sold a growler?</button>
+
+     <div class="progress">
+       <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: {{keg.kegLevel}}%">
+         <span class="sr-only">40% Complete (success)</span>
+       </div>
+     </div>
    </div>
   `
 })
@@ -22,6 +28,9 @@ export class ShowKegProgressComponent {
   }
   reduceByGrowler(): void {
     this.keg.kegLevel -=4;
+  }
+  kegLevelByColor(keg): void {
+    var fillLevel = keg.kegLevel;
   }
 
 ///http://arjunu.com/2016/01/basic-angular-2-off-click-directive/ USE THIS WEBSITE TO HELP WITH CLICK HIDE/SHOW
